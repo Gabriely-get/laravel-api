90ec9b5dec6449648c574e67c3538a14d2917365
Gabriely Willian Cristovão dos Santos <gabriely.santos@gbmlogistica.com.br>
2025-02-05 17:10:49 -0300

    refac: create user test

--- tests/Unit/UserControllerTest.php
+++ tests/Unit/UserControllerTest.php
@@ -1,11 +1,11 @@
 <?php
 
 namespace Tests\Unit;
-
+use Mockery\Undefined;
+use Tests\TestCase;
 use Illuminate\Http\JsonResponse;
 use \Mockery;
 use App\Repositories\UserRepository;
-use PHPUnit\Framework\TestCase;
 use Illuminate\Http\Request;
 use App\Services\UserService;
 use App\Http\Controllers\UserController;
@@ -14,40 +14,34 @@ class UserControllerTest extends TestCase
 {
     public function test_should_create_user_with_mock(): void
      {
-        try {
-            $request = new Request([
-                'name' => 'Gabriely W.',
-                'email' => 'duda@gmail.com',
-                'password' => 'Abc1234@!#$'
-            ]);
+        ///prepare Mock
+        $request = new Request([
+            'name' => 'Teste W.',
+            'email' => 'teste@gmail.com',
+            'password' => 'Abc1234@!#$'
+        ]);
 
-            $userServiceMock =$this->createMock(UserService::class);
+        $userServiceMock =$this->createMock(UserService::class);
+        $userServiceMock
+        ->expects($this->once())
+        ->method('create')
+        ->with($request->all())
+        ->willReturn(new JsonResponse([
+            'name' => 'Teste W.',
+            'email' => 'teste@gmail.com',
+            'id' => 7
+        ], 201));
 
-            //$user = $this->$userServiceMock->create($request);
+        //instance
+        $controller = new UserController($userServiceMock);
+        $response = $controller->store($request);
 
-            $userServiceMock
-            ->method('create')
-            ->with($request->all())
-            ->willReturn(new JsonResponse([
-                'name' => 'Gabriely W.',
-                'email' => 'duda@gmail.com',
-                'id' => 7
-            ], 201));
+        //assert
+        $responseData = json_decode($response->getContent(), true)['original'];
 
-            $controller = new UserController($userServiceMock);
-            $response = $controller->store($request);
-
-            $responseData = json_decode($response->getContent(), true);
-
-            // Validando o retorno
-            $this->assertEquals(201, $response->getStatusCode());
-            $this->assertEquals('Gabriely W.', $responseData['name']);
-            $this->assertEquals('duda@gmail.com', $responseData['email']);
-            $this->assertEquals(7, $responseData['id']);
-
-            $this->assertEquals($request, $response);
-        } catch(\Exception $e) {
-            echo $e;
-        }
+        $this->assertEquals(201, $response->getStatusCode());
+        $this->assertEquals('Teste W.', $responseData['name']);
+        $this->assertEquals('teste@gmail.com', $responseData['email']);
+        $this->assertEquals(7, $responseData['id']);
     }
 }

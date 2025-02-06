6779558aaecdeb253acc005135e969635e9bd984
Gabriely Willian Cristovão dos Santos <gabriely.santos@gbmlogistica.com.br>
2025-02-05 22:51:52 -0300

    feat: test get all users

--- tests/Unit/UserControllerTest.php
+++ tests/Unit/UserControllerTest.php
@@ -12,6 +12,48 @@ use App\Http\Controllers\UserController;
 
 class UserControllerTest extends TestCase
 {
+    public function test_should_get_all_users_empty(): void
+    {
+        $userServiceMock =$this->createMock(UserService::class);
+        $userServiceMock
+        ->expects($this->once())
+        ->method('all')
+        ->willReturn(new JsonResponse([]));
+
+        $controller = new UserController($userServiceMock);
+        $response = $controller->all();
+
+        //assert
+        $responseData = json_decode($response->getContent(), true)['original'];
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals([], $responseData);
+    }
+
+    public function test_should_get_all_users(): void
+    {
+        $userServiceMock =$this->createMock(UserService::class);
+        $userServiceMock
+        ->expects($this->once())
+        ->method('all')
+        ->willReturn(new JsonResponse([
+            'name' => 'Teste W.',
+            'email' => 'teste@gmail.com',
+            'id' => 7
+        ], 200));
+
+        $controller = new UserController($userServiceMock);
+        $response = $controller->all();
+
+        //assert
+        $responseData = json_decode($response->getContent(), true)['original'];
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals('Teste W.', $responseData['name']);
+        $this->assertEquals('teste@gmail.com', $responseData['email']);
+        $this->assertEquals(7, $responseData['id']);
+    }
+
     public function test_should_create_user_with_mock(): void
      {
         ///prepare Mock

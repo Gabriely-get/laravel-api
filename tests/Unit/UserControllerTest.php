6c03c15f77f828b7392a13e2b15f5ddede0d52bf
Gabriely Willian Cristovão dos Santos <gabriely.santos@gbmlogistica.com.br>
2025-02-05 22:51:33 -0300

    feat: test create user validate

--- tests/Unit/UserControllerTest.php
+++ tests/Unit/UserControllerTest.php
@@ -44,4 +44,95 @@ class UserControllerTest extends TestCase
         $this->assertEquals('teste@gmail.com', $responseData['email']);
         $this->assertEquals(7, $responseData['id']);
     }
+
+    public function test_should_return_error_when_name_is_missing(): void
+    {
+        $request = new Request([
+            'name' => '',
+            'email' => 'teste@gmail.com',
+            'password' => 'Abc1234@!#$'
+        ]);
+
+        $userServiceMock =$this->createMock(UserService::class);
+        $userServiceMock
+        ->expects($this->never())
+        ->method('create');
+
+        $controller = new UserController($userServiceMock);
+        $response = $controller->store($request);
+
+        $responseData = json_decode($response->getContent(), true)['errors'];
+
+        $this->assertEquals(400, $response->getStatusCode());
+        $this->assertEquals('O campo nome é obrigatório.', $responseData['name'][0]);
+    }
+
+    public function test_should_return_error_when_email_is_missing(): void
+    {
+        $request = new Request([
+            'name' => 'Teste',
+            'email' => '',
+            'password' => 'Abc1234@!#$'
+        ]);
+
+        $userServiceMock =$this->createMock(UserService::class);
+        $userServiceMock
+        ->expects($this->never())
+        ->method('create');
+
+        $controller = new UserController($userServiceMock);
+        $response = $controller->store($request);
+
+        $responseData = json_decode($response->getContent(), true)['errors'];
+
+        $this->assertEquals(400, $response->getStatusCode());
+        $this->assertEquals('O campo email é obrigatório.', $responseData['email'][0]);
+    }
+
+    public function test_should_return_error_when_password_is_missing(): void
+    {
+        $request = new Request([
+            'name' => 'Teste',
+            'email' => 'teste@gmail.com',
+            'password' => ''
+        ]);
+
+        $userServiceMock =$this->createMock(UserService::class);
+        $userServiceMock
+        ->expects($this->never())
+        ->method('create');
+
+        $controller = new UserController($userServiceMock);
+        $response = $controller->store($request);
+
+        $responseData = json_decode($response->getContent(), true)['errors'];
+
+        $this->assertEquals(400, $response->getStatusCode());
+        $this->assertEquals('O campo senha é obrigatório', $responseData['password'][0]);
+    }
+
+    public function test_should_return_error_when_all_fields_is_missing(): void
+    {
+        $request = new Request([
+            'name' => '',
+            'email' => '',
+            'password' => ''
+        ]);
+
+        $userServiceMock =$this->createMock(UserService::class);
+        $userServiceMock
+        ->expects($this->never())
+        ->method('create');
+
+        $controller = new UserController($userServiceMock);
+        $response = $controller->store($request);
+
+        $responseData = json_decode($response->getContent(), true)['errors'];
+
+        $this->assertEquals(400, $response->getStatusCode());
+        $this->assertEquals('O campo nome é obrigatório.', $responseData['name'][0]);
+        $this->assertEquals('O campo email é obrigatório.', $responseData['email'][0]);
+        $this->assertEquals('O campo senha é obrigatório', $responseData['password'][0]);
+    }
+
 }
